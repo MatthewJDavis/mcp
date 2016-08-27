eabad74176e6c05c64fe25bfdda690ba3f0c7f23
Matt Davis <matthewdavis111@gmail.com>
2016-08-27 17:12:02 +0100

    One simple web app PowerShell script added

---
+++ 70-533-Azure-infra-solutions/web-apps/simple-web-app/New-SimpleWebApp.ps1
@@ -0,0 +1,20 @@
+﻿##Create a simple web app
+
+$resourceGroupName = "one-web-app-RG"
+$location = "north europe"
+
+#Login to Azure account
+#Login-AzureRmAccount
+#Select-AzureRmSubscription -SubscriptionName 
+
+##Create the resource group
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location -Tags @{Project="Simple Web App";Course="70-533"}
+
+#Create App Service plan
+New-AzureRmAppServicePlan -ResourceGroupName "one-web-app-RG" -Name "Simple-Web-App" -Location $location -Tier Basic -NumberofWorkers 1 -WorkerSize "Small" 
+
+#Create web app
+New-AzureRmWebApp -ResourceGroupName "one-web-app-RG" -Name "matt-test-web-app" -Location $location -AppServicePlan "Simple-Web-App"
+
+
+    
