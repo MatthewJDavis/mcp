0fedcf73825d97552abbc0b56a80c9ae257083cc
Matt Davis <matthewdavis111@gmail.com>
2016-08-28 14:02:27 +0100

    Created simple web VS project

--- 70-533-Azure-infra-solutions/web-apps/simple-web-app/New-SimpleWebApp.ps1
+++ 70-533-Azure-infra-solutions/web-apps/simple-web-app/New-SimpleWebApp.ps1
@@ -1,5 +1,6 @@
 ﻿##Create a simple web app
 #Will create a one web app with a staging deployment slot
+#Creates a web job
 
 $resourceGroupName = "simple-web-app-RG"
 $location = "north europe"
@@ -25,5 +26,9 @@ New-AzureRmWebApp -ResourceGroupName $resourceGroupName  -Name $webAppName -Loca
 
 #Create 2 deployment slots
 New-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $webAppName  -Slot $slot1
+
+#Swap slots
+#Swap-AzureRmWebAppSlot -SourceSlotName $slot1 -DestinationSlotName "Production" -ResourceGroupName $resourceGroupName -Name $webAppName
+
 
 # Remove-AzureRmResourceGroup -Name simple-web-app-RG -Force   
---
+++ 70-533-Azure-infra-solutions/web-apps/simple-web-app/simple-web-app/simple-web-app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(simple_web_app.Startup))]
+namespace simple_web_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
