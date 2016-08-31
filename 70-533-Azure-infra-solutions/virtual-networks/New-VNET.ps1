2043c73353972a0d39a3d6a04589acbf187a06df
Matt Davis <matthewdavis111@gmail.com>
2016-08-31 22:37:47 +0100

    Set up vnet project. Added PowerShell script, json template files for 2 vnets and a visio diagram

---
+++ 70-533-Azure-infra-solutions/virtual-networks/New-VNET.ps1
@@ -0,0 +1,5 @@
+﻿#Create a resource group and deploy a VNET into it via a JSON template file
+
+New-AzureRmResourceGroup -Name "vnet-RG" -Location northeurope -Tag  @{Project="VNET";Course="70-533"}
+
+New-AzureRmResourceGroupDeployment -Name "vnet-deploy" -ResourceGroupName "vnet-RG" -Mode Incremental -TemplateParameterFile ""
