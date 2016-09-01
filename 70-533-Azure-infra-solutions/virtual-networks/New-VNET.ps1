f4f87113ac31dcb8871780a0ff3025943a17f0f1
Matt Davis <matthewdavis111@gmail.com>
2016-09-01 20:37:41 +0100

    Added remove resouce group command to PowerShell script

--- 70-533-Azure-infra-solutions/virtual-networks/New-VNET.ps1
+++ 70-533-Azure-infra-solutions/virtual-networks/New-VNET.ps1
@@ -1,5 +1,12 @@
 ﻿#Create a resource group and deploy a VNET into it via a JSON template file
 
-New-AzureRmResourceGroup -Name "vnet-RG" -Location northeurope -Tag  @{Project="VNET";Course="70-533"}
+$rgName = "vnet-RG"
+$location = "northeurope"
 
-New-AzureRmResourceGroupDeployment -Name "vnet-deploy" -ResourceGroupName "vnet-RG" -Mode Incremental -TemplateParameterFile ""
+New-AzureRmResourceGroup -Name $rgName -Location $location -Tag  @{Project="VNET";Course="70-533"}
+
+New-AzureRmResourceGroupDeployment -Name "office-vnet-deploy" -ResourceGroupName $rgName -Mode Incremental -TemplateFile "C:\git\mcp\70-533-Azure-infra-solutions\virtual-networks\office-azuredeploy.json"
+
+New-AzureRmResourceGroupDeployment -Name "host-vnet-deploy" -ResourceGroupName $rgName -Mode Incremental -TemplateFile "C:\git\mcp\70-533-Azure-infra-solutions\virtual-networks\host-azuredeploy.json"
+
+#Remove-AzureRmResourceGroup -Name $rgName -Force
